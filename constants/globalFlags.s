f8dad73fa645baee0c3e386f564dba33e1ceafbb
Matthew Stewart <stewartmatthew6@gmail.com>
2015-07-16 13:25:00 -0400

    Move constant definitions to constants/

---
+++ constants/globalFlags.s
@@ -0,0 +1,19 @@
+; Set after nayru cutscene
+.define GLOBALFLAG_INTRO_DONE	$0a
+
+; Set when all crystals destroyed
+.define GLOBALFLAG_D3_CRYSTALS	$0f
+.define GLOBALFLAG_SAVED_NAYRU $11
+
+; 14 - when set, plays nayru's music on impa's house screen
+
+; Should double-check this one
+.define GLOBALFLAG_TALKED_TO_RAFTON $15
+
+; When set, the triforce intro does not play
+.define GLOBALFLAG_PREGAME_INTRO_DONE $21
+
+; When set, link does not "fall" into the game
+.define GLOBALFLAG_NO_FALL_ON_START $35
+
+.define GLOBALFLAG_RALPH_ENTERED_PORTAL $40
