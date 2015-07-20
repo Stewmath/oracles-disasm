4edc89f9929c7abcdccb09f3602b08b3aa9b22ec
Matthew Stewart <stewartmatthew6@gmail.com>
2015-07-19 23:30:44 -0400

    Move areaData label

--- data/areas.s
+++ data/areas.s
@@ -1,722 +1,724 @@
 ; Format: Flags1, Flags2, Unique, Gfxdata, Palette, Tileset, Layout Group, Animation
 
+areaData: ; 4:4f9c
+
 ; Area 0x00
-.db $0f $01
-.db UNIQGFXH_01
-.db GFXH_40
-.db PALH_10
-.db $00 $00 $00
+	.db $0f $01
+	.db UNIQGFXH_01
+	.db GFXH_40
+	.db PALH_10
+	.db $00 $00 $00
 
 ; Area 0x01
-.db $0f $01
-.db UNIQGFXH_03
-.db GFXH_40
-.db PALH_22
-.db $01 $00 $01
+	.db $0f $01
+	.db UNIQGFXH_03
+	.db GFXH_40
+	.db PALH_22
+	.db $01 $00 $01
 
 ; Area 0x02
-.db $0f $01
-.db UNIQGFXH_04
-.db GFXH_40
-.db PALH_24
-.db $02 $00 $01
+	.db $0f $01
+	.db UNIQGFXH_04
+	.db GFXH_40
+	.db PALH_24
+	.db $02 $00 $01
 
 ; Area 0x03
-.db $0f $01
-.db UNIQGFXH_05
-.db GFXH_40
-.db PALH_2d
-.db $14 $00 $01
+	.db $0f $01
+	.db UNIQGFXH_05
+	.db GFXH_40
+	.db PALH_2d
+	.db $14 $00 $01
 
 ; Area 0x04
-.db $0f $01
-.db UNIQGFXH_06
-.db GFXH_40
-.db PALH_12
-.db $03 $00 $03
+	.db $0f $01
+	.db UNIQGFXH_06
+	.db GFXH_40
+	.db PALH_12
+	.db $03 $00 $03
 
 ; Area 0x05
-.db $0f $01
-.db UNIQGFXH_07
-.db GFXH_40
-.db PALH_14
-.db $04 $00 $01
+	.db $0f $01
+	.db UNIQGFXH_07
+	.db GFXH_40
+	.db PALH_14
+	.db $04 $00 $01
 
 ; Area 0x06
-.db $0f $01
-.db UNIQGFXH_08
-.db GFXH_40
-.db PALH_16
-.db $05 $00 $04
+	.db $0f $01
+	.db UNIQGFXH_08
+	.db GFXH_40
+	.db PALH_16
+	.db $05 $00 $04
 
 ; Area 0x07
-.db $0f $01
-.db UNIQGFXH_0b
-.db GFXH_40
-.db PALH_2e
-.db $19 $01 $01
+	.db $0f $01
+	.db UNIQGFXH_0b
+	.db GFXH_40
+	.db PALH_2e
+	.db $19 $01 $01
 
 ; Area 0x08
-.db $0f $01
-.db UNIQGFXH_09
-.db GFXH_40
-.db PALH_28
-.db $06 $00 $01
+	.db $0f $01
+	.db UNIQGFXH_09
+	.db GFXH_40
+	.db PALH_28
+	.db $06 $00 $01
 
 ; Area 0x09
-.db $0f $01
-.db UNIQGFXH_09
-.db GFXH_40
-.db PALH_28
-.db $06 $01 $01
+	.db $0f $01
+	.db UNIQGFXH_09
+	.db GFXH_40
+	.db PALH_28
+	.db $06 $01 $01
 
 ; Area 0x0a
-.db $0f $01
-.db UNIQGFXH_0c
-.db GFXH_40
-.db PALH_18
-.db $08 $00 $01
+	.db $0f $01
+	.db UNIQGFXH_0c
+	.db GFXH_40
+	.db PALH_18
+	.db $08 $00 $01
 
 ; Area 0x0b
-.db $0f $01
-.db UNIQGFXH_0c
-.db GFXH_40
-.db PALH_18
-.db $08 $01 $01
+	.db $0f $01
+	.db UNIQGFXH_0c
+	.db GFXH_40
+	.db PALH_18
+	.db $08 $01 $01
 
 ; Area 0x0c
-.db $0f $01
-.db UNIQGFXH_0e
-.db GFXH_40
-.db PALH_1c
-.db $0a $00 $03
+	.db $0f $01
+	.db UNIQGFXH_0e
+	.db GFXH_40
+	.db PALH_1c
+	.db $0a $00 $03
 
 ; Area 0x0d
-.db $0f $01
-.db UNIQGFXH_10
-.db GFXH_40
-.db PALH_26
-.db $0d $00 $06
+	.db $0f $01
+	.db UNIQGFXH_10
+	.db GFXH_40
+	.db PALH_26
+	.db $0d $00 $06
 
 ; Area 0x0e
-.db $0f $01
-.db UNIQGFXH_10
-.db GFXH_40
-.db PALH_26
-.db $0d $01 $06
+	.db $0f $01
+	.db UNIQGFXH_10
+	.db GFXH_40
+	.db PALH_26
+	.db $0d $01 $06
 
 ; Area 0x0f
-.db $0f $01
-.db UNIQGFXH_10
-.db GFXH_40
-.db PALH_26
-.db $0d $03 $06
+	.db $0f $01
+	.db UNIQGFXH_10
+	.db GFXH_40
+	.db PALH_26
+	.db $0d $03 $06
 
 ; Area 0x10
-.db $0f $81
-.db UNIQGFXH_02
-.db GFXH_41
-.db PALH_11
-.db $0f $02 $00
+	.db $0f $81
+	.db UNIQGFXH_02
+	.db GFXH_41
+	.db PALH_11
+	.db $0f $02 $00
 
 ; Area 0x11
-.db $0f $81
-.db UNIQGFXH_02
-.db GFXH_41
-.db PALH_11
-.db $0f $03 $00
+	.db $0f $81
+	.db UNIQGFXH_02
+	.db GFXH_41
+	.db PALH_11
+	.db $0f $03 $00
 
 ; Area 0x12
-.db $0f $81
-.db UNIQGFXH_02
-.db GFXH_41
-.db PALH_2f
-.db $0f $02 $00
+	.db $0f $81
+	.db UNIQGFXH_02
+	.db GFXH_41
+	.db PALH_2f
+	.db $0f $02 $00
 
 ; Area 0x13
-.db $0f $81
-.db UNIQGFXH_03
-.db GFXH_41
-.db PALH_23
-.db $01 $02 $01
+	.db $0f $81
+	.db UNIQGFXH_03
+	.db GFXH_41
+	.db PALH_23
+	.db $01 $02 $01
 
 ; Area 0x14
-.db $0f $81
-.db UNIQGFXH_04
-.db GFXH_41
-.db PALH_25
-.db $02 $02 $01
+	.db $0f $81
+	.db UNIQGFXH_04
+	.db GFXH_41
+	.db PALH_25
+	.db $02 $02 $01
 
 ; Area 0x15
-.db $0f $81
-.db UNIQGFXH_04
-.db GFXH_41
-.db PALH_25
-.db $02 $03 $01
+	.db $0f $81
+	.db UNIQGFXH_04
+	.db GFXH_41
+	.db PALH_25
+	.db $02 $03 $01
 
 ; Area 0x16
-.db $0f $81
-.db UNIQGFXH_05
-.db GFXH_41
-.db PALH_33
-.db $14 $02 $01
+	.db $0f $81
+	.db UNIQGFXH_05
+	.db GFXH_41
+	.db PALH_33
+	.db $14 $02 $01
 
 ; Area 0x17
-.db $0f $81
-.db UNIQGFXH_06
-.db GFXH_41
-.db PALH_13
-.db $03 $02 $03
+	.db $0f $81
+	.db UNIQGFXH_06
+	.db GFXH_41
+	.db PALH_13
+	.db $03 $02 $03
 
 ; Area 0x18
-.db $0f $81
-.db UNIQGFXH_07
-.db GFXH_41
-.db PALH_15
-.db $04 $02 $01
+	.db $0f $81
+	.db UNIQGFXH_07
+	.db GFXH_41
+	.db PALH_15
+	.db $04 $02 $01
 
 ; Area 0x19
-.db $0f $81
-.db UNIQGFXH_08
-.db GFXH_41
-.db PALH_17
-.db $05 $02 $04
+	.db $0f $81
+	.db UNIQGFXH_08
+	.db GFXH_41
+	.db PALH_17
+	.db $05 $02 $04
 
 ; Area 0x1a
-.db $0f $81
-.db UNIQGFXH_09
-.db GFXH_41
-.db PALH_29
-.db $06 $02 $01
+	.db $0f $81
+	.db UNIQGFXH_09
+	.db GFXH_41
+	.db PALH_29
+	.db $06 $02 $01
 
 ; Area 0x1b
-.db $0f $81
-.db UNIQGFXH_09
-.db GFXH_41
-.db PALH_29
-.db $06 $03 $01
+	.db $0f $81
+	.db UNIQGFXH_09
+	.db GFXH_41
+	.db PALH_29
+	.db $06 $03 $01
 
 ; Area 0x1c
-.db $0f $91
-.db UNIQGFXH_09
-.db GFXH_41
-.db PALH_29
-.db $06 $02 $01
+	.db $0f $91
+	.db UNIQGFXH_09
+	.db GFXH_41
+	.db PALH_29
+	.db $06 $02 $01
 
 ; Area 0x1d
-.db $0f $81
-.db UNIQGFXH_0a
-.db GFXH_41
-.db PALH_2a
-.db $07 $02 $01
+	.db $0f $81
+	.db UNIQGFXH_0a
+	.db GFXH_41
+	.db PALH_2a
+	.db $07 $02 $01
 
 ; Area 0x1e
-.db $0f $81
-.db UNIQGFXH_0d
-.db GFXH_41
-.db PALH_1b
-.db $09 $02 $02
+	.db $0f $81
+	.db UNIQGFXH_0d
+	.db GFXH_41
+	.db PALH_1b
+	.db $09 $02 $02
 
 ; Area 0x1f
-.db $0f $81
-.db UNIQGFXH_0e
-.db GFXH_41
-.db PALH_1d
-.db $0a $02 $03
+	.db $0f $81
+	.db UNIQGFXH_0e
+	.db GFXH_41
+	.db PALH_1d
+	.db $0a $02 $03
 
 ; Area 0x20
-.db $0f $81
-.db UNIQGFXH_0f
-.db GFXH_41
-.db PALH_1f
-.db $0b $02 $05
+	.db $0f $81
+	.db UNIQGFXH_0f
+	.db GFXH_41
+	.db PALH_1f
+	.db $0b $02 $05
 
 ; Area 0x21
-.db $0f $81
-.db UNIQGFXH_11
-.db GFXH_41
-.db PALH_27
-.db $0e $02 $01
+	.db $0f $81
+	.db UNIQGFXH_11
+	.db GFXH_41
+	.db PALH_27
+	.db $0e $02 $01
 
 ; Area 0x22
-.db $0f $02
-.db UNIQGFXH_00
-.db GFXH_70
-.db PALH_30
-.db $11 $00 $07
+	.db $0f $02
+	.db UNIQGFXH_00
+	.db GFXH_70
+	.db PALH_30
+	.db $11 $00 $07
 
 ; Area 0x23
-.db $0f $02
-.db UNIQGFXH_00
-.db GFXH_70
-.db PALH_30
-.db $11 $01 $07
+	.db $0f $02
+	.db UNIQGFXH_00
+	.db GFXH_70
+	.db PALH_30
+	.db $11 $01 $07
 
 ; Area 0x24
-.db $0f $02
-.db UNIQGFXH_00
-.db GFXH_70
-.db PALH_30
-.db $11 $03 $07
+	.db $0f $02
+	.db UNIQGFXH_00
+	.db GFXH_70
+	.db PALH_30
+	.db $11 $03 $07
 
 ; Area 0x25
-.db $0f $02
-.db UNIQGFXH_00
-.db GFXH_71
-.db PALH_31
-.db $12 $00 $ff
+	.db $0f $02
+	.db UNIQGFXH_00
+	.db GFXH_71
+	.db PALH_31
+	.db $12 $00 $ff
 
 ; Area 0x26
-.db $0f $82
-.db UNIQGFXH_00
-.db GFXH_70
-.db PALH_30
-.db $11 $02 $07
+	.db $0f $82
+	.db UNIQGFXH_00
+	.db GFXH_70
+	.db PALH_30
+	.db $11 $02 $07
 
 ; Area 0x27
-.db $af $18
-.db UNIQGFXH_00
-.db GFXH_6e
-.db PALH_60
-.db $13 $04 $ff
+	.db $af $18
+	.db UNIQGFXH_00
+	.db GFXH_6e
+	.db PALH_60
+	.db $13 $04 $ff
 
 ; Area 0x28
-.db $1f $04
-.db UNIQGFXH_00
-.db GFXH_7b
-.db PALH_70
-.db $15 $01 $10
+	.db $1f $04
+	.db UNIQGFXH_00
+	.db GFXH_7b
+	.db PALH_70
+	.db $15 $01 $10
 
 ; Area 0x29
-.db $1f $04
-.db UNIQGFXH_00
-.db GFXH_7b
-.db PALH_71
-.db $15 $01 $10
+	.db $1f $04
+	.db UNIQGFXH_00
+	.db GFXH_7b
+	.db PALH_71
+	.db $15 $01 $10
 
 ; Area 0x2a
-.db $1f $84
-.db UNIQGFXH_00
-.db GFXH_7b
-.db PALH_72
-.db $15 $01 $10
+	.db $1f $84
+	.db UNIQGFXH_00
+	.db GFXH_7b
+	.db PALH_72
+	.db $15 $01 $10
 
 ; Area 0x2b
-.db $1f $04
-.db UNIQGFXH_00
-.db GFXH_7b
-.db PALH_70
-.db $15 $03 $10
+	.db $1f $04
+	.db UNIQGFXH_00
+	.db GFXH_7b
+	.db PALH_70
+	.db $15 $03 $10
 
 ; Area 0x2c
-.db $1f $04
-.db UNIQGFXH_00
-.db GFXH_7b
-.db PALH_72
-.db $15 $03 $10
+	.db $1f $04
+	.db UNIQGFXH_00
+	.db GFXH_7b
+	.db PALH_72
+	.db $15 $03 $10
 
 ; Area 0x2d
-.db $1f $04
-.db UNIQGFXH_00
-.db GFXH_7c
-.db PALH_73
-.db $16 $01 $10
+	.db $1f $04
+	.db UNIQGFXH_00
+	.db GFXH_7c
+	.db PALH_73
+	.db $16 $01 $10
 
 ; Area 0x2e
-.db $1f $04
-.db UNIQGFXH_00
-.db GFXH_7c
-.db PALH_73
-.db $16 $03 $10
+	.db $1f $04
+	.db UNIQGFXH_00
+	.db GFXH_7c
+	.db PALH_73
+	.db $16 $03 $10
 
 ; Area 0x2f
-.db $1f $04
-.db UNIQGFXH_00
-.db GFXH_7d
-.db PALH_73
-.db $16 $03 $10
+	.db $1f $04
+	.db UNIQGFXH_00
+	.db GFXH_7d
+	.db PALH_73
+	.db $16 $03 $10
 
 ; Area 0x30
-.db $1f $04
-.db UNIQGFXH_00
-.db GFXH_7e
-.db PALH_75
-.db $17 $01 $10
+	.db $1f $04
+	.db UNIQGFXH_00
+	.db GFXH_7e
+	.db PALH_75
+	.db $17 $01 $10
 
 ; Area 0x31
-.db $1f $44
-.db UNIQGFXH_00
-.db GFXH_7f
-.db PALH_76
-.db $18 $03 $10
+	.db $1f $44
+	.db UNIQGFXH_00
+	.db GFXH_7f
+	.db PALH_76
+	.db $18 $03 $10
 
 ; Area 0x32
-.db $1f $44
-.db UNIQGFXH_00
-.db GFXH_7f
-.db PALH_79
-.db $18 $01 $10
+	.db $1f $44
+	.db UNIQGFXH_00
+	.db GFXH_7f
+	.db PALH_79
+	.db $18 $01 $10
 
 ; Area 0x33
-.db $1f $44
-.db UNIQGFXH_00
-.db GFXH_7f
-.db PALH_79
-.db $18 $03 $10
+	.db $1f $44
+	.db UNIQGFXH_00
+	.db GFXH_7f
+	.db PALH_79
+	.db $18 $03 $10
 
 ; Area 0x34
-.db $20 $08
-.db UNIQGFXH_00
-.db GFXH_60
-.db PALH_40
-.db $20 $04 $10
+	.db $20 $08
+	.db UNIQGFXH_00
+	.db GFXH_60
+	.db PALH_40
+	.db $20 $04 $10
 
 ; Area 0x35
-.db $2d $88
-.db UNIQGFXH_00
-.db GFXH_60
-.db PALH_4d
-.db $20 $04 $10
+	.db $2d $88
+	.db UNIQGFXH_00
+	.db GFXH_60
+	.db PALH_4d
+	.db $20 $04 $10
 
 ; Area 0x36
-.db $21 $08
-.db UNIQGFXH_00
-.db GFXH_61
-.db PALH_41
-.db $21 $04 $10
+	.db $21 $08
+	.db UNIQGFXH_00
+	.db GFXH_61
+	.db PALH_41
+	.db $21 $04 $10
 
 ; Area 0x37
-.db $22 $88
-.db UNIQGFXH_00
-.db GFXH_62
-.db PALH_42
-.db $22 $04 $10
+	.db $22 $88
+	.db UNIQGFXH_00
+	.db GFXH_62
+	.db PALH_42
+	.db $22 $04 $10
 
 ; Area 0x38
-.db $23 $08
-.db UNIQGFXH_00
-.db GFXH_63
-.db PALH_43
-.db $23 $04 $10
+	.db $23 $08
+	.db UNIQGFXH_00
+	.db GFXH_63
+	.db PALH_43
+	.db $23 $04 $10
 
 ; Area 0x39
-.db $24 $08
-.db UNIQGFXH_00
-.db GFXH_64
-.db PALH_44
-.db $24 $04 $10
+	.db $24 $08
+	.db UNIQGFXH_00
+	.db GFXH_64
+	.db PALH_44
+	.db $24 $04 $10
 
 ; Area 0x3a
-.db $25 $08
-.db UNIQGFXH_00
-.db GFXH_65
-.db PALH_45
-.db $25 $04 $10
+	.db $25 $08
+	.db UNIQGFXH_00
+	.db GFXH_65
+	.db PALH_45
+	.db $25 $04 $10
 
 ; Area 0x3b
-.db $26 $08
-.db UNIQGFXH_00
-.db GFXH_66
-.db PALH_46
-.db $26 $05 $10
+	.db $26 $08
+	.db UNIQGFXH_00
+	.db GFXH_66
+	.db PALH_46
+	.db $26 $05 $10
 
 ; Area 0x3c
-.db $2c $88
-.db UNIQGFXH_00
-.db GFXH_66
-.db PALH_4c
-.db $26 $05 $10
+	.db $2c $88
+	.db UNIQGFXH_00
+	.db GFXH_66
+	.db PALH_4c
+	.db $26 $05 $10
 
 ; Area 0x3d
-.db $2c $c8
-.db UNIQGFXH_00
-.db GFXH_66
-.db PALH_61
-.db $26 $05 $13
+	.db $2c $c8
+	.db UNIQGFXH_00
+	.db GFXH_66
+	.db PALH_61
+	.db $26 $05 $13
 
 ; Area 0x3e
-.db $27 $08
-.db UNIQGFXH_00
-.db GFXH_67
-.db PALH_47
-.db $27 $05 $15
+	.db $27 $08
+	.db UNIQGFXH_00
+	.db GFXH_67
+	.db PALH_47
+	.db $27 $05 $15
 
 ; Area 0x3f
-.db $27 $48
-.db UNIQGFXH_00
-.db GFXH_67
-.db PALH_62
-.db $27 $05 $15
+	.db $27 $48
+	.db UNIQGFXH_00
+	.db GFXH_67
+	.db PALH_62
+	.db $27 $05 $15
 
 ; Area 0x40
-.db $28 $88
-.db UNIQGFXH_00
-.db GFXH_68
-.db PALH_48
-.db $28 $05 $10
+	.db $28 $88
+	.db UNIQGFXH_00
+	.db GFXH_68
+	.db PALH_48
+	.db $28 $05 $10
 
 ; Area 0x41
-.db $28 $88
-.db UNIQGFXH_14
-.db GFXH_68
-.db PALH_63
-.db $28 $05 $10
+	.db $28 $88
+	.db UNIQGFXH_14
+	.db GFXH_68
+	.db PALH_63
+	.db $28 $05 $10
 
 ; Area 0x42
-.db $28 $c8
-.db UNIQGFXH_00
-.db GFXH_68
-.db PALH_64
-.db $28 $05 $13
+	.db $28 $c8
+	.db UNIQGFXH_00
+	.db GFXH_68
+	.db PALH_64
+	.db $28 $05 $13
 
 ; Area 0x43
-.db $2f $90
-.db UNIQGFXH_00
-.db GFXH_69
-.db PALH_49
-.db $29 $04 $10
+	.db $2f $90
+	.db UNIQGFXH_00
+	.db GFXH_69
+	.db PALH_49
+	.db $29 $04 $10
 
 ; Area 0x44
-.db $2f $90
-.db UNIQGFXH_00
-.db GFXH_69
-.db PALH_59
-.db $29 $04 $10
+	.db $2f $90
+	.db UNIQGFXH_00
+	.db GFXH_69
+	.db PALH_59
+	.db $29 $04 $10
 
 ; Area 0x45
-.db $af $98
-.db UNIQGFXH_00
-.db GFXH_69
-.db PALH_49
-.db $29 $04 $14
+	.db $af $98
+	.db UNIQGFXH_00
+	.db GFXH_69
+	.db PALH_49
+	.db $29 $04 $14
 
 ; Area 0x46
-.db $2a $08
-.db UNIQGFXH_00
-.db GFXH_6a
-.db PALH_4a
-.db $2a $05 $10
+	.db $2a $08
+	.db UNIQGFXH_00
+	.db GFXH_6a
+	.db PALH_4a
+	.db $2a $05 $10
 
 ; Area 0x47
-.db $2a $08
-.db UNIQGFXH_00
-.db GFXH_6a
-.db PALH_67
-.db $2a $05 $10
+	.db $2a $08
+	.db UNIQGFXH_00
+	.db GFXH_6a
+	.db PALH_67
+	.db $2a $05 $10
 
 ; Area 0x48
-.db $2b $08
-.db UNIQGFXH_00
-.db GFXH_60
-.db PALH_4b
-.db $20 $04 $10
+	.db $2b $08
+	.db UNIQGFXH_00
+	.db GFXH_60
+	.db PALH_4b
+	.db $20 $04 $10
 
 ; Area 0x49
-.db $2b $08
-.db UNIQGFXH_00
-.db GFXH_60
-.db PALH_66
-.db $20 $04 $10
+	.db $2b $08
+	.db UNIQGFXH_00
+	.db GFXH_60
+	.db PALH_66
+	.db $20 $04 $10
 
 ; Area 0x4a
-.db $2b $48
-.db UNIQGFXH_00
-.db GFXH_60
-.db PALH_65
-.db $20 $04 $11
+	.db $2b $48
+	.db UNIQGFXH_00
+	.db GFXH_60
+	.db PALH_65
+	.db $20 $04 $11
 
 ; Area 0x4b
-.db $3d $a8
-.db UNIQGFXH_00
-.db GFXH_6d
-.db PALH_52
-.db $32 $04 $ff
+	.db $3d $a8
+	.db UNIQGFXH_00
+	.db GFXH_6d
+	.db PALH_52
+	.db $32 $04 $ff
 
 ; Area 0x4c
-.db $31 $28
-.db UNIQGFXH_00
-.db GFXH_6d
-.db PALH_51
-.db $32 $04 $12
+	.db $31 $28
+	.db UNIQGFXH_00
+	.db GFXH_6d
+	.db PALH_51
+	.db $32 $04 $12
 
 ; Area 0x4d
-.db $32 $28
-.db UNIQGFXH_00
-.db GFXH_6d
-.db PALH_52
-.db $32 $04 $12
+	.db $32 $28
+	.db UNIQGFXH_00
+	.db GFXH_6d
+	.db PALH_52
+	.db $32 $04 $12
 
 ; Area 0x4e
-.db $34 $28
-.db UNIQGFXH_00
-.db GFXH_6d
-.db PALH_53
-.db $32 $04 $12
+	.db $34 $28
+	.db UNIQGFXH_00
+	.db GFXH_6d
+	.db PALH_53
+	.db $32 $04 $12
 
 ; Area 0x4f
-.db $35 $28
-.db UNIQGFXH_00
-.db GFXH_6d
-.db PALH_53
-.db $32 $04 $12
+	.db $35 $28
+	.db UNIQGFXH_00
+	.db GFXH_6d
+	.db PALH_53
+	.db $32 $04 $12
 
 ; Area 0x50
-.db $36 $28
-.db UNIQGFXH_00
-.db GFXH_6d
-.db PALH_50
-.db $32 $05 $12
+	.db $36 $28
+	.db UNIQGFXH_00
+	.db GFXH_6d
+	.db PALH_50
+	.db $32 $05 $12
 
 ; Area 0x51
-.db $3c $a8
-.db UNIQGFXH_00
-.db GFXH_6d
-.db PALH_56
-.db $32 $05 $12
+	.db $3c $a8
+	.db UNIQGFXH_00
+	.db GFXH_6d
+	.db PALH_56
+	.db $32 $05 $12
 
 ; Area 0x52
-.db $37 $a8
-.db UNIQGFXH_00
-.db GFXH_6d
-.db PALH_54
-.db $32 $05 $12
+	.db $37 $a8
+	.db UNIQGFXH_00
+	.db GFXH_6d
+	.db PALH_54
+	.db $32 $05 $12
 
 ; Area 0x53
-.db $38 $a8
-.db UNIQGFXH_00
-.db GFXH_6d
-.db PALH_55
-.db $32 $05 $12
+	.db $38 $a8
+	.db UNIQGFXH_00
+	.db GFXH_6d
+	.db PALH_55
+	.db $32 $05 $12
 
 ; Area 0x54
-.db $3b $28
-.db UNIQGFXH_00
-.db GFXH_6d
-.db PALH_52
-.db $32 $04 $ff
+	.db $3b $28
+	.db UNIQGFXH_00
+	.db GFXH_6d
+	.db PALH_52
+	.db $32 $04 $ff
 
 ; Area 0x55
-.db $2e $18
-.db UNIQGFXH_00
-.db GFXH_7b
-.db PALH_70
-.db $15 $05 $10
+	.db $2e $18
+	.db UNIQGFXH_00
+	.db GFXH_7b
+	.db PALH_70
+	.db $15 $05 $10
 
 ; Area 0x56
-.db $2f $10
-.db UNIQGFXH_00
-.db GFXH_7b
-.db PALH_70
-.db $15 $05 $10
+	.db $2f $10
+	.db UNIQGFXH_00
+	.db GFXH_7b
+	.db PALH_70
+	.db $15 $05 $10
 
 ; Area 0x57
-.db $2f $12
-.db UNIQGFXH_00
-.db GFXH_7b
-.db PALH_77
-.db $15 $05 $10
+	.db $2f $12
+	.db UNIQGFXH_00
+	.db GFXH_7b
+	.db PALH_77
+	.db $15 $05 $10
 
 ; Area 0x58
-.db $2e $18
-.db UNIQGFXH_00
-.db GFXH_7c
-.db PALH_73
-.db $16 $05 $10
+	.db $2e $18
+	.db UNIQGFXH_00
+	.db GFXH_7c
+	.db PALH_73
+	.db $16 $05 $10
 
 ; Area 0x59
-.db $2f $10
-.db UNIQGFXH_00
-.db GFXH_7c
-.db PALH_73
-.db $16 $05 $10
+	.db $2f $10
+	.db UNIQGFXH_00
+	.db GFXH_7c
+	.db PALH_73
+	.db $16 $05 $10
 
 ; Area 0x5a
-.db $2e $18
-.db UNIQGFXH_00
-.db GFXH_7e
-.db PALH_74
-.db $17 $05 $10
+	.db $2e $18
+	.db UNIQGFXH_00
+	.db GFXH_7e
+	.db PALH_74
+	.db $17 $05 $10
 
 ; Area 0x5b
-.db $2f $50
-.db UNIQGFXH_00
-.db GFXH_7f
-.db PALH_76
-.db $18 $05 $10
+	.db $2f $50
+	.db UNIQGFXH_00
+	.db GFXH_7f
+	.db PALH_76
+	.db $18 $05 $10
 
 ; Area 0x5c
-.db $3f $30
-.db UNIQGFXH_00
-.db GFXH_6d
-.db PALH_57
-.db $32 $05 $12
+	.db $3f $30
+	.db UNIQGFXH_00
+	.db GFXH_6d
+	.db PALH_57
+	.db $32 $05 $12
 
 ; Area 0x5d
-.db $3f $30
-.db UNIQGFXH_00
-.db GFXH_6d
-.db PALH_58
-.db $32 $05 $12
+	.db $3f $30
+	.db UNIQGFXH_00
+	.db GFXH_6d
+	.db PALH_58
+	.db $32 $05 $12
 
 ; Area 0x5e
-.db $3e $38
-.db UNIQGFXH_00
-.db GFXH_6d
-.db PALH_57
-.db $32 $05 $12
+	.db $3e $38
+	.db UNIQGFXH_00
+	.db GFXH_6d
+	.db PALH_57
+	.db $32 $05 $12
 
 ; Area 0x5f
-.db $4f $41
-.db UNIQGFXH_13
-.db GFXH_42
-.db PALH_2b
-.db $10 $01 $08
+	.db $4f $41
+	.db UNIQGFXH_13
+	.db GFXH_42
+	.db PALH_2b
+	.db $10 $01 $08
 
 ; Area 0x60
-.db $4f $c1
-.db UNIQGFXH_00
-.db GFXH_43
-.db PALH_2c
-.db $10 $03 $08
+	.db $4f $c1
+	.db UNIQGFXH_00
+	.db GFXH_43
+	.db PALH_2c
+	.db $10 $03 $08
 
 ; Area 0x61
-.db $4f $d1
-.db UNIQGFXH_13
-.db GFXH_42
-.db PALH_2c
-.db $10 $03 $08
+	.db $4f $d1
+	.db UNIQGFXH_13
+	.db GFXH_42
+	.db PALH_2c
+	.db $10 $03 $08
 
 ; Area 0x62
-.db $4f $41
-.db UNIQGFXH_12
-.db GFXH_42
-.db PALH_32
-.db $1a $01 $08
+	.db $4f $41
+	.db UNIQGFXH_12
+	.db GFXH_42
+	.db PALH_32
+	.db $1a $01 $08
 
 ; Area 0x63
-.db $1f $04
-.db UNIQGFXH_00
-.db GFXH_7c
-.db PALH_78
-.db $16 $01 $10
+	.db $1f $04
+	.db UNIQGFXH_00
+	.db GFXH_7c
+	.db PALH_78
+	.db $16 $01 $10
 
 ; Area 0x64
-.db $1f $04
-.db UNIQGFXH_00
-.db GFXH_7c
-.db PALH_78
-.db $16 $03 $10
+	.db $1f $04
+	.db UNIQGFXH_00
+	.db GFXH_7c
+	.db PALH_78
+	.db $16 $03 $10
 
 ; Area 0x65
-.db $2e $18
-.db UNIQGFXH_00
-.db GFXH_7c
-.db PALH_78
-.db $16 $05 $10
+	.db $2e $18
+	.db UNIQGFXH_00
+	.db GFXH_7c
+	.db PALH_78
+	.db $16 $05 $10
 
 ; Area 0x66
-.db $1f $04
-.db UNIQGFXH_00
-.db GFXH_7d
-.db PALH_73
-.db $16 $02 $10
+	.db $1f $04
+	.db UNIQGFXH_00
+	.db GFXH_7d
+	.db PALH_73
+	.db $16 $02 $10
