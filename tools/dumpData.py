bba2976e58660b983d99ab53e2a5be7710c25329
Matthew Stewart <stewartmatthew6@gmail.com>
2015-07-23 15:02:20 -0400

    Tool to dump tile mappings, might need to redo this from scratch

--- tools/dumpData.py
+++ tools/dumpData.py
@@ -2,23 +2,23 @@
 import sys
 import StringIO
 
-index = sys.argv[0].find('/') 
+index = sys.argv[0].find('/')
 if index == -1:
-	directory = ''
+    directory = ''
 else:
-	directory = sys.argv[0][:index+1]
-execfile(directory+'common.py')
+    directory = sys.argv[0][:index + 1]
+execfile(directory + 'common.py')
 
 if len(sys.argv) < 4:
-	print 'Usage: ' + sys.argv[0] + ' romfile startaddress size'
-        print 'Output goes to stout'
-	sys.exit()
+    print 'Usage: ' + sys.argv[0] + ' romfile startaddress size'
+    print 'Output goes to stout'
+    sys.exit()
 
-romFile = open(sys.argv[1],'rb')
+romFile = open(sys.argv[1], 'rb')
 rom = bytearray(romFile.read())
 
 startAddress = int(sys.argv[2])
-endAddress = startAddress+int(sys.argv[3])
+endAddress = startAddress + int(sys.argv[3])
 
 address = startAddress
 
@@ -26,17 +26,17 @@ bytesThisRow = 0
 output = StringIO.StringIO()
 
 while address < endAddress:
-	if bytesThisRow == 0:
-		output.write('.db ')
-	if bytesThisRow == 7:
-		output.write(wlahex(rom[address],2)+'\n')
-	else:
-		output.write(wlahex(rom[address],2)+' ')
+    if bytesThisRow == 0:
+        output.write('.db ')
+    if bytesThisRow == 7:
+        output.write(wlahex(rom[address], 2) + '\n')
+    else:
+        output.write(wlahex(rom[address], 2) + ' ')
 
-	address+=1
-	bytesThisRow+=1
-	if bytesThisRow == 8:
-		bytesThisRow = 0
+    address += 1
+    bytesThisRow += 1
+    if bytesThisRow == 8:
+        bytesThisRow = 0
 
 output.seek(0)
 print(output.read())
---
+++ tools/dumpTileMappings.py
@@ -0,0 +1,71 @@
+import sys
+import StringIO
+
+index = sys.argv[0].find('/')
+if index == -1:
+    directory = ''
+else:
+    directory = sys.argv[0][:index+1]
+execfile(directory+'common.py')
+
+if len(sys.argv) < 2:
+    print 'Usage: ' + sys.argv[0] + ' romfile'
+
+romFile = open(sys.argv[1], 'rb')
+rom = bytearray(romFile.read())
+
+class TileMapping:
+    def __init__(self):
+        self.indexDataOffset = 0
+        self.attributeDataOffset = 0
+
+# Constants
+tileMappingBank = 0x18
+tileMappingTable = bankedAddress(tileMappingBank, 0x4004)
+tileIndexDataPointer = bankedAddress(tileMappingBank, 0x4000)
+tileAttributeDataPointer = bankedAddress(tileMappingBank, 0x4002)
+numTileMappings = 0x852
+
+tileIndexDataAddr = bankedAddress(tileMappingBank, read16(rom, tileIndexDataPointer))
+tileAttributeDataAddr = bankedAddress(tileMappingBank, read16(rom, tileAttributeDataPointer))
+
+tileIndexData = rom[tileIndexDataAddr:]
+tileAttributeData = rom[tileAttributeDataAddr:]
+
+
+outFile = open('tileMappings.bin','wb')
+
+highestIndexData = 0
+highestAttributeData = 0
+
+for i in range(numTileMappings):
+    addr = tileMappingTable + i*3
+    indexDataOffset = rom[addr]
+    indexDataOffset |= (rom[addr+1]&0xf0)<<4
+    indexDataOffset *= 4
+    indexDataAddr = tileIndexDataAddr+indexDataOffset
+
+    attributeDataOffset = rom[addr+2]
+    attributeDataOffset |= (rom[addr+1]&0xf)<<8
+    attributeDataOffset *= 4
+    attributeDataAddr = tileAttributeDataAddr+attributeDataOffset
+
+    if highestIndexData < indexDataAddr+4:
+        highestIndexData = indexDataAddr+4
+    if highestAttributeData < attributeDataAddr+4:
+        highestAttributeData = attributeDataAddr+4
+
+    data = bytearray()
+    for i in range(4):
+        data.append(rom[indexDataAddr+i])
+    for i in range(4):
+        data.append(rom[attributeDataAddr+i])
+    outFile.write(data)
+
+outFile.close()
+
+# Debug stuff
+print 'Pointer   data starts at ' + hex(tileMappingTable) + ', ends at ' + hex(tileMappingTable+numTileMappings*3)
+print 'Index     data starts at ' + hex(tileIndexDataAddr) + ', ends at ' + hex(highestIndexData)
+print 'Attribute data starts at ' + hex(tileAttributeDataAddr) + ', ends at ' + hex(highestAttributeData)
+
