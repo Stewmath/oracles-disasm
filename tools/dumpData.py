af7b4da162eefed694f9bd87a33769a7c4162792
Matthew Stewart <stewartmatthew6@gmail.com>
2015-08-03 23:27:57 -0400

    Went over some bank 0 functions

--- tools/dumpData.py
+++ tools/dumpData.py
@@ -10,7 +10,7 @@ else:
 execfile(directory + 'common.py')
 
 if len(sys.argv) < 4:
-    print 'Usage: ' + sys.argv[0] + ' romfile startaddress size'
+    print 'Usage: ' + sys.argv[0] + ' romfile startaddress size [bytesPerLine]'
     print 'Output goes to stout'
     sys.exit()
 
@@ -20,6 +20,11 @@ rom = bytearray(romFile.read())
 startAddress = int(sys.argv[2])
 endAddress = startAddress + int(sys.argv[3])
 
+if len(sys.argv) > 4:
+    bytesPerLine = int(sys.argv[4])
+else:
+    bytesPerLine = 8
+
 address = startAddress
 
 bytesThisRow = 0
@@ -28,14 +33,14 @@ output = StringIO.StringIO()
 while address < endAddress:
     if bytesThisRow == 0:
         output.write('.db ')
-    if bytesThisRow == 7:
+    if bytesThisRow == bytesPerLine-1:
         output.write(wlahex(rom[address], 2) + '\n')
     else:
         output.write(wlahex(rom[address], 2) + ' ')
 
     address += 1
     bytesThisRow += 1
-    if bytesThisRow == 8:
+    if bytesThisRow == bytesPerLine:
         bytesThisRow = 0
 
 output.seek(0)
