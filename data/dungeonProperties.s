6bf6d032ebdf13b5045f70bb75fb7127e1073db2
Matthew Stewart <stewartmatthew6@gmail.com>
2016-01-15 11:21:26 -0500

    Weird naming

--- data/dungeonProperties.s
+++ data/dungeonProperties.s
@@ -1,9 +1,9 @@
-roomDungeonPropertiesGroupTable: ; $4dca
-	.dw roomDungeonPropertiesGroup4Data
-	.dw roomDungeonPropertiesGroup5Data
+dungeonRoomPropertiesGroupTable: ; $4dca
+	.dw dungeonRoomPropertiesGroup4Data
+	.dw dungeonRoomPropertiesGroup5Data
 
-roomDungeonPropertiesGroup4Data: ; 4dce
+dungeonRoomPropertiesGroup4Data: ; 4dce
 	.incbin "rooms/group4DungeonProperties.bin"
-roomDungeonPropertiesGroup5Data: ; 4ece
+dungeonRoomPropertiesGroup5Data: ; 4ece
 	.incbin "rooms/group5DungeonProperties.bin"
 
