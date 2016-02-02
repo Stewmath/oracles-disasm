5a6344a0bab47257e116af34ddd188a95c389579
Matthew Stewart <stewartmatthew6@gmail.com>
2016-02-01 19:48:22 -0500

    Sign text table

---
+++ data/signText.s
@@ -0,0 +1,74 @@
+; @addr{$1b784}
+signTextGroupTable:
+        .dw signTextGroup0Data
+        .dw signTextGroup1Data
+        .dw signTextGroup2Data
+        .dw signTextGroup3Data
+        .dw signTextGroup4Data
+        .dw signTextGroup5Data
+        .dw signTextGroup6Data
+        .dw signTextGroup7Data
+
+; @addr{$1b794}
+signTextGroup0Data:
+        .db $35 $2a $01
+        .db $51 $3d $05
+        .db $46 $19 $06
+        .db $22 $1d $07
+        .db $32 $48 $0d
+        .db $47 $57 $0e
+        .db $37 $6a $0f
+        .db $42 $64 $10
+        .db $28 $24 $11
+        .db $57 $50 $12
+        .db $42 $68 $14
+        .db $51 $55 $15
+        .db $15 $59 $16
+        .db $32 $83 $1c
+        .db $36 $47 $1d
+        .db $00
+; @addr{$1b7c2}
+signTextGroup1Data:
+        .db $54 $39 $03
+        .db $53 $33 $04
+        .db $24 $1d $08
+        .db $44 $3a $09
+        .db $32 $48 $0d
+        .db $33 $53 $13
+        .db $31 $57 $17
+        .db $23 $89 $18
+        .db $31 $49 $1e
+        .db $00
+; @addr{$1b7de}
+signTextGroup2Data:
+        .db $52 $e9 $00
+        .db $46 $fd $0a
+        .db $46 $ff $0a
+        .db $44 $c4 $19
+        .db $35 $b1 $1a
+        .db $58 $a1 $1b
+        .db $38 $d1 $1f
+        .db $00
+; @addr{$1b7f4}
+signTextGroup3Data:
+        .db $15 $1e $0b
+        .db $34 $4e $0c
+        .db $44 $c4 $19
+        .db $35 $b1 $1a
+        .db $58 $a1 $1b
+        .db $38 $d1 $1f
+        .db $00
+; @addr{$1b807}
+signTextGroup4Data:
+        .db $62 $01 $02
+        .db $3c $02 $02
+        .db $32 $02 $02
+        .db $44 $03 $02
+        .db $00
+; @addr{$1b814}
+signTextGroup5Data:
+        .db $6c $bf $20
+signTextGroup6Data:
+signTextGroup7Data:
+        .db $00
+
