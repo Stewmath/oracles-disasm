c1f20989660cce92b51ddd6adb4b876e576518b3
Matthew Stewart <stewartmatthew6@gmail.com>
2016-07-03 16:39:35 -0400

    Separate past chest / sign graphics, make uncmp gfx header labels use indices

--- data/gfxDataBank19_1.s
+++ data/gfxDataBank19_1.s
@@ -5,5 +5,7 @@
 	m_GfxDataSimple gfx_item_icons_3 ; $064400
 
 	m_GfxDataSimple gfx_animations_3 ; $064600
-	m_GfxDataSimple gfx_sign
+
+	m_GfxDataSimple gfx_past_chest
+	m_GfxDataSimple gfx_past_sign
 
