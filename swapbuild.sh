e55dbf17d4fc8f854596a073e9df8202cc482205
Matthew Stewart <stewartmatthew6@gmail.com>
2016-12-23 18:05:34 -0500

    Let swapbuild.sh work when build directories don't exist yet

--- swapbuild.sh
+++ swapbuild.sh
@@ -3,18 +3,18 @@
 # Change the build mode from "vanilla" (exact copy) to fully modifiable, or
 # vice versa.
 
-[ ! -d build ] && echo "No build directory exists" && exit 1
-
 if grep -q 'BUILD_VANILLA = true' Makefile; then
     echo "Switching to modifiable (non-precompressed) mode"
 
     sed -i 's/BUILD_VANILLA = true/BUILD_VANILLA = false/' Makefile
 
     [ -f build/no_use_precompressed ] && exit 0
-    [ ! -d build_e ] && mkdir -v build_e
+    [ ! -d build_e ] && mkdir -v build_e && touch build_e/no_use_precompressed
     [ -d build_v ] \
         && echo "ERROR: Folder 'build_v' exists already, not swapping build mode" \
         && exit 1
+
+    [ ! -d build ] && mkdir -v build && touch build/use_precompressed
 
     mv -v build build_v
     mv -v build_e build
@@ -25,10 +25,12 @@ elif grep -q 'BUILD_VANILLA = false' Makefile; then
     sed -i 's/BUILD_VANILLA = false/BUILD_VANILLA = true/' Makefile
 
     [ -f build/use_precompressed ] && exit 0
-    [ ! -d build_v ] && mkdir -v build_v
+    [ ! -d build_v ] && mkdir -v build_v && touch build_v/use_precompressed
     [ -d build_e ] \
         && echo "ERROR: Folder 'build_e' exists already, not swapping build mode" \
         && exit 1
+
+    [ ! -d build ] && mkdir -v build && touch build/no_use_precompressed
 
     mv -v build build_e
     mv -v build_v build
