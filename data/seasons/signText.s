0120e39f01c70b6af4f9b0756cbaaccf903dd009
Matthew Stewart <stewartmatthew6@gmail.com>
2019-11-30 19:52:59 -0500

    Factor out "interactable tiles" code to work with seasons

---
+++ data/seasons/signText.s
@@ -0,0 +1,52 @@
+signTextGroupTable:
+        .dw signTextGroup0Data
+        .dw signTextGroup1Data
+        .dw signTextGroup2Data
+        .dw signTextGroup3Data
+        .dw signTextGroup4Data
+        .dw signTextGroup5Data
+        .dw signTextGroup6Data
+        .dw signTextGroup7Data
+
+signTextGroup0Data:
+signTextGroup3Data:
+        .db $43 $d9 $00
+        .db $52 $4d $01
+        .db $17 $0b $03
+        .db $53 $1f $04
+        .db $27 $97 $05
+        .db $45 $73 $0a
+        .db $27 $fc $0b
+        .db $28 $35 $0c
+        .db $45 $7f $0d
+        .db $13 $b4 $0e
+        .db $55 $82 $0f
+        .db $17 $82 $10
+        .db $26 $9b $11
+        .db $41 $63 $12
+        .db $22 $7b $13
+        .db $34 $54 $14
+        .db $56 $d7 $15
+        .db $12 $c4 $16
+        .db $23 $2c $17
+        .db $38 $bb $18
+        .db $27 $55 $19
+        .db $25 $f4 $1a
+        .db $23 $b9 $1b
+        .db $33 $f6 $1d
+        .db $23 $68 $1e
+        .db $00
+signTextGroup1Data:
+        .db $27 $45 $07
+        .db $42 $59 $08
+        .db $46 $14 $09
+        .db $00
+signTextGroup2Data:
+signTextGroup4Data:
+        .db $17 $04 $02
+        .db $00
+signTextGroup5Data:
+        .db $82 $12 $1c
+signTextGroup6Data:
+signTextGroup7Data:
+        .db $00
