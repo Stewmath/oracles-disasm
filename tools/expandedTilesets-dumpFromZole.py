a7f56ef46f361886557c49663a0edd4493fd9ca3
Matthew Stewart <stewartmatthew6@gmail.com>
2020-02-18 21:12:44 -0500

    Added dungeon layouts & music IDs to ZOLE dumper

--- tools/expandedTilesets-dumpFromZole.py
+++ tools/expandedTilesets-dumpFromZole.py
@@ -1,19 +1,38 @@
 import sys, os
 from common import *
 
-if len(sys.argv) < 2:
-    print('Usage: ' + sys.argv[0] + ' romfile [-agrt]')
+if len(sys.argv) < 3:
+    print('Usage: ' + sys.argv[0] + ' romfile [options]')
     print('Dumps data from a ZOLE rom into the disassembly.')
-    print('')
+    print('\nOptions:')
     print('\t-a: All of the below')
+    print('\t-d: Dungeon layouts')
     print('\t-g: Tileset Graphics')
-    print('\t-i: Area IDs (TODO)')
-    print('\t-m: Music IDs (TODO)')
+    print('\t-i: Area IDs')
+    print('\t-m: Music IDs')
     print('\t-r: Room Layouts')
     print('\t-t: Tileset Mappings')
+    print('\nExample: "%s rom.gbc -g -t" dumps tileset graphics and mappings from rom.gbc.' % sys.argv[0])
+    print('\nOther things:\
+            \n\tWarps: Use tools/dumpWarps.py.\
+            \n\tObjects: Use tools/dumpObjects.py.')
+    print('\nStuff from ZOLE that\'s not (yet) dumped by this script:\
+            \n\t- Chests\
+            \n\t- Palettes\
+            \n\t- Miniboss portal warps\
+            \n\t- Link\'s start location\
+            \n\t- Area flags\
+            \n\t- Essence warp location\
+            \n\t- Enemy edits\
+            \n\t- Sign & text changes\
+            \n\t- Treetop edits\
+            \n\t- Dungeon room flags\
+            \n\t- ZOSE scripts\
+            \n\t- ASM patches')
+
     sys.exit()
 
-filename = sys.argv[-1]
+filename = sys.argv[1]
 
 romFile = open(filename, 'rb')
 rom = bytearray(romFile.read())
@@ -24,6 +43,8 @@ if romIsAges(rom):
     NUM_TILEMAPS = 0x2c
     areaData = 0x10f9c
     areaIdAddr = 0x112d4
+    musicTable = 0x1095c
+    dungeonLayoutAddr = 0x4fce
     game = 'ages'
 elif romIsSeasons(rom):
     NUM_AREAS = 0xcf # TODO: handle seasonal areas properly
@@ -36,17 +57,33 @@ else:
     sys.exit(1)
 
 
-args = sys.argv[1:]
+args = []
+
+for arg in sys.argv[2:]:
+    if arg[0] == '-':
+        for c in arg[1:]:
+            args.append('-' + c)
+    else:
+        print('Unrecognized argument "%s".' % arg)
+        sys.exit(1)
 
 if '-a' in args:
+    args.append('-d')
     args.append('-g')
     args.append('-i')
     args.append('-m')
     args.append('-r')
     args.append('-t')
 
+if '-d' in args:
+    print('Dumping dungeon layouts.')
+    data = rom[dungeonLayoutAddr : dungeonLayoutAddr+0x680]
+    f = open('rooms/%s/dungeonLayouts.bin' % game, 'wb')
+    f.write(data)
+    f.close()
+
 if '-g' in args:
-    # Dump tileset graphics
+    print('Dumping tileset graphics.')
     for tileset in range(0,NUM_AREAS):
         gfxAddr = 0x181000 + tileset * 0x1000
         gfxData = rom[gfxAddr:gfxAddr+0x1000]
@@ -55,7 +92,7 @@ if '-g' in args:
         outFile.close()
 
 if '-i' in args:
-    # Dump area IDs
+    print('Dumping Area IDs.')
     for group in range(0,6):
         addr = bankedAddress(4, read16(rom, areaIdAddr + 2 * group))
         data = rom[addr : addr+0x100]
@@ -63,9 +100,19 @@ if '-i' in args:
         f.write(data)
         f.close()
 
+if '-m' in args:
+    print('Dumping Music IDs.')
+    for group in range(0,6):
+        addr = bankedAddress(4, read16(rom, musicTable + group*2))
+        data = rom[addr : addr+0x100]
+        f = open('audio/%s/group%dIDs.bin' % (game, group), 'wb')
+        f.write(data)
+        f.close()
+
 if '-r' in args:
     # Dump room layouts. The way these are stored is unchanged in the disassembly, but ZOLE expanded
     # them. Useful for porting ZOLE projects to the disassembly.
+    print('Dumping room layouts.')
     for group in range(0,6):
         # ZOLE seems to ignore "layout groups"? (which might be for the best...)
         if group == 1:
@@ -92,9 +139,9 @@ if '-r' in args:
             layoutAddr += roomSize
 
 if '-t' in args:
-    # Dump tilemaps.
     # ZOLE's tilemaps aren't actually fully separated by area ID, for some reason. So we do this
     # ourselves.
+    print('Dumping tilemaps.')
     tilemapAddr = 0x201000
     for area in range(0,NUM_AREAS):
         tilemap = rom[areaData + area*8 + 5]
